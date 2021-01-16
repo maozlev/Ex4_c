39815d6ec5cb656cc842c54a8d2fd0c561525257
maoz lev <maoz20011111@gmail.com>
2021-01-16 23:20:05 +0200

    final.03 update

--- frequency.c
+++ frequency.c
@@ -9,7 +9,7 @@ typedef struct node {
  char letter;
  long unsigned int count;
  boolean end_word;
- boolean is_leaf;
+ boolean not_leaf;
  struct node* children[NUM_LETTERS];
 } node;
 
@@ -33,7 +33,7 @@ node* new_node(char letter) {
     p->letter = letter;
     p->count = 0;
     p->end_word = FALSE;
-    p->is_leaf = FALSE;
+    p->not_leaf = FALSE;
 
     for (i = 0; i < NUM_LETTERS; ++i) {
         (p->children)[i] = NULL;
@@ -47,7 +47,7 @@ void free_node(node* p) {
     if(p == NULL) {
         return;
     }
-    if(p->is_leaf) {
+    if(p->not_leaf) {
         for (int i = 0; i <NUM_LETTERS; i++) {
            free_node((p->children)[i]);
         }
@@ -58,19 +58,20 @@ void free_node(node* p) {
 
 trie* new_trie(trie* t) {
     int i;
+
     if(!(t=(trie*)malloc(sizeof(trie)))) {
         printf("Not enough memory!\n");
         exit(1);
     }
-      
-    for (i=0; i<NUM_LETTERS; ++i) {
-        t->children[i]=NULL;
+    
+    for (i = 0; i < NUM_LETTERS; ++i) {
+        t->children[i] = NULL;
     }
 
-    t->current=NULL;
-    t->empty=TRUE;
-    t->max_length=0;
-    t->word=NULL;
+    t->current = NULL;
+    t->empty = TRUE;
+    t->max_length = 0;
+    t->word = NULL;
     
     return t;
 }
@@ -95,7 +96,7 @@ int read_character(trie* root, int c, int word_length) {
     int index;
 
     if(!isalpha(c)) {
-        if (c == ' ' || c == '\t' || c == '\n' || c== EOF) {
+        if (c == ' ' || c == '\t' || c == '\n' || c == EOF) {
             close_word(root);
             return word_length;
         }
@@ -114,7 +115,7 @@ int read_character(trie* root, int c, int word_length) {
         root->empty = FALSE;
     }
     else {
-        root->current->is_leaf = TRUE;
+        root->current->not_leaf = TRUE;
         if (root->current->children[index] == NULL)
             root->current->children[index] = new_node(c);
         root->current=root->current->children[index];
@@ -122,21 +123,21 @@ int read_character(trie* root, int c, int word_length) {
     return word_length;
 }
 
-trie* read_text() {
+trie* scan_txt() {
     int c;
     int word_length=0;
     trie* root;
     root = new_trie(root);
 
-    while(EOF != (c=getchar())) {
-        word_length = read_character(root, c, word_length);
+    while((c=getchar()) != EOF) {
+        word_length = read_character(root, c, word_length); // returns the length of the current word
         if (word_length>root->max_length)
             root->max_length = word_length;
-        if(!isalpha(c))
+        if (!isalpha(c))
             word_length=0;
     }
 
-   free(root->word);
+   free(root->word); // Free the rest of the memory
 
     if (!(root->word=(char*)malloc(1+sizeof(char)*(root->max_length)))) {
         printf("Not enough memory!\n");
@@ -145,19 +146,19 @@ trie* read_text() {
     return root;
 }
 
-void print_words_reverse(trie* root) {
-    static int p=0;
+void reverse_words(trie* root) {
+    static int p = 0;
     int i;
     node* current;
     root->word[p++] = root->current->letter;
 
-    if (root->current->is_leaf) {
-        for (i = NUM_LETTERS - 1; i >= 0; --i) {
+    if (root->current->not_leaf) {
+        for (i = NUM_LETTERS - 1; i > -1; --i) {
             if (root->current->children[i] == NULL)
                 continue;
             current = root->current;
             root->current = root->current->children[i];
-            print_words_reverse(root);
+            reverse_words(root);
             root->current = current;
         }
     }
@@ -169,6 +170,7 @@ void print_words_reverse(trie* root) {
         --p;
         return;
     }
+
     if (root->current->end_word) {
         root->word[p] = '\0';
         printf("%s %ld\n",root->word,root->current->count);
@@ -176,18 +178,19 @@ void print_words_reverse(trie* root) {
     --p;
 }
 
-void print_trie_reverse(trie* root) {
+void reverse_trie(trie* root) {
     int i;
+
     if (root == NULL)
         return;
     if (is_empty(root))
         return;
 
-    for (i = NUM_LETTERS - 1; i >= 0; --i) {
+    for (i = NUM_LETTERS - 1; i > -1; --i) {
         if (root->children[i] == NULL)
             continue;
         root->current = root->children[i];
-        print_words_reverse(root);
+        reverse_words(root);
     }
 }
 
@@ -200,7 +203,7 @@ void print_words(trie* root) {
         root->word[p] = '\0';
         printf("%s %ld\n",root->word,root->current->count);
     }
-    if (root->current->is_leaf) {
+    if (root->current->not_leaf) {
         for( i= 0; i < NUM_LETTERS; ++i) {
             if (root->current->children[i] == NULL)
                 continue;
@@ -224,7 +227,7 @@ void print_trie(trie* root) {
     if (is_empty(root))
         return;
 
-    for (i=0; i<NUM_LETTERS; ++i) {
+    for (i = 0; i < NUM_LETTERS; ++i) {
         if (root->children[i] == NULL)
             continue;
         root->current = root->children[i];
@@ -236,41 +239,37 @@ void free_trie(trie* t) {
     int i;
     if (t == NULL)
         return;
-    for(i=0;i<NUM_LETTERS; ++i) {
+    for(i = 0;i < NUM_LETTERS; ++i) {
         free_node(t->children[i]);
     }
     free(t->word);
     free(t);
 }
 
-void usage(char* program_name,char* message) {
-    printf("\n%s\n\n",message);
-    printf("USAGE:\n\t%s: %s\n\n", program_name, message);
-    exit(1);
-}
-
-boolean should_reverse(int argc, char* argv[]) {
-    if (argc > 2)
-        usage(argv[0],"Wrong number of arguments");
-
-    if ( (argc == 2) && (argv[1][0]=='r' || argv[1][0]=='R'))
+boolean to_rev(int argc, char* argv[]) { 
+    if (argc == 2 && argv[1][0] == 'r')
         return TRUE;
-
+    
     if (argc == 1)
         return FALSE;
-    
-    usage(argv[0],"Bad command line arguments.");
-    return FALSE;
+
+    if (argc > 2) { // example: "./frequency q r < ../input.txt"
+        printf("\nToo many arguments!\n");
+        exit(1);
+    }
+
+    printf("\nBad command\n");
+    exit(1);
 }
 
 int main(int argc, char* argv[]) {
     trie* root;
-    boolean rev = should_reverse(argc,argv);
-    root = read_text();
-    if (rev)
-        print_trie_reverse(root);
-    else
+    boolean rev = to_rev(argc,argv);
+    root = scan_txt();
+    if (!rev)
         print_trie(root);
+    else
+        reverse_trie(root);
 
     free_trie(root);
 
